5d497690d30322270522527de12a267e0366c96a
xcxpcxkes <77536236+xcxpcxkes@users.noreply.github.com>
2022-10-06 17:08:10 +0800

    feat: add error pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Dashboard } from './pages/index'
+import { Dashboard, Error } from './pages/index'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" index element={<Dashboard />} />
+                <Route path="*" element={<Error />} />
             </Routes>
         </BrowserRouter>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,17 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        theme: {
-            colors: {
-                divider: '#E4E5E7',
-                white: '#FFFFFF',
-                background: '#F4EDE2',
-                foreground: '#F5F5F7',
-                maintext: '#3A4555',
-                accent1: '#ECCCC4',
-                accent2: '#DADBD5',
-                accent3: '#AEBED0',
-            },
-            extend: {},
+        colors: {
+            divider: '#E4E5E7',
+            white: '#FFFFFF',
+            background: '#F4EDE2',
+            foreground: '#F5F5F7',
+            maintext: '#3A4555',
+            accent1: '#ECCCC4',
+            accent2: '#DADBD5',
+            accent3: '#AEBED0',
         },
-        plugins: [],
+        extend: {},
     },
+    plugins: [],
 }
