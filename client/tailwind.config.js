8cae38109e19d6a1ead2646fddd0b92eaab40e7b
xcxpcxkes <77536236+xcxpcxkes@users.noreply.github.com>
2022-10-06 16:44:11 +0800

    feat: added page for dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,19 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        theme: {
+            colors: {
+                divider: '#E4E5E7',
+                white: '#FFFFFF',
+                background: '#F4EDE2',
+                foreground: '#F5F5F7',
+                maintext: '#3A4555',
+                accent1: '#ECCCC4',
+                accent2: '#DADBD5',
+                accent3: '#AEBED0',
+            },
+            extend: {},
+        },
+        plugins: [],
+    },
 }
