8b273bf4dce21680941943432870cfdc425d51a1
xcxpcxkes <77536236+xcxpcxkes@users.noreply.github.com>
2022-10-16 21:21:40 +0800

    feat: forgetpwd resetpwd page

--- client/src/pages/ForgetResetPwd.jsx
+++ client/src/pages/ForgetResetPwd.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
-const ForgetResetPwd = () => {
+export default function ForgetResetPwd() {
     const {
         register,
         handleSubmit,
@@ -11,46 +11,68 @@ const ForgetResetPwd = () => {
     const onSubmit = (data) => console.log(data)
 
     return (
-        <div>
-            <div className="p-24 space-y-5">
-                <div>
-                    <h3 className="text-2xl leading-6 font-medium text-gray-900">Forget Password</h3>
-                    <p className="mt-1 max-w-2xl text-sm text-gray-500">
-                        Use the username that you created with Habilidad
-                    </p>
-                </div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                        <label
-                            htmlFor="first-name"
-                            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-                        >
-                            Username
-                        </label>
-                        <div className="mt-1 sm:mt-0 sm:col-span-2">
-                            <input
-                                type="text"
-                                name="username"
-                                id="username"
-                                className="max-w-lg block w-full shadow-sm bg-navbarfooter sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
-                                {...register('username', { required: true })}
-                            />
+        <div className="min-h-screen bg-background flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Forget or Reset Password</h2>
+            </div>
+
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                        <div>
+                            <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                                Username
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="username"
+                                    name="username"
+                                    type="text"
+                                    placeholder="Enter Username"
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    {...register('username', { required: 'Please enter your username' })}
+                                />
+                                {errors.username ? (
+                                    <span className="text-sm text-red-500">{errors.username.message}</span>
+                                ) : null}
+                            </div>
                         </div>
-                    </div>
-                    <div className="pt-5">
-                        <div className="flex justify-end">
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                Email
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    placeholder="Enter Email"
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    {...register('email', {
+                                        required: 'Please enter your email',
+                                        pattern: {
+                                            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                            message: 'Please enter a valid email',
+                                        },
+                                    })}
+                                />
+                                {errors.email ? (
+                                    <span className="text-sm text-red-500">{errors.email.message}</span>
+                                ) : null}
+                            </div>
+                        </div>
+                        <div>
                             <button
                                 type="submit"
-                                className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-black"
+                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-black bg-navbarfooter"
                             >
-                                Save
+                                Submit to receive email
                             </button>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     )
 }
-
-export default ForgetResetPwd
