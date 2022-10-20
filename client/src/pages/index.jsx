9dd930a13c7a750b14d283edb774c22787d68f24
Jasticar <jasmin.yap06@gmail.com>
2022-10-20 15:39:39 +0800

    added profile page

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Dashboard from './Dashboard'
 import Error from './Error'
 import Register from './Register'
+import Profile from './Profile'
 
-export { Dashboard, Error, Register }
+export { Dashboard, Error, Register, Profile }
