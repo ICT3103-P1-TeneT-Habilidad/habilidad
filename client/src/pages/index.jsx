c4704b24e8bbeb9100ce8411e8b8833da912112e
Jasticar <jasmin.yap06@gmail.com>
2022-10-24 03:56:12 +0800

    feat: created course creation page and route

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -4,5 +4,6 @@ import ForgetResetPwd from './ForgetResetPwd'
 import Login from './Login'
 import Register from './Register'
 import Profile from './Profile'
+import CreateCourse from './CreateCourse'
 
-export { Dashboard, Error, ForgetResetPwd, Login, Register, Profile }
+export { Dashboard, Error, ForgetResetPwd, Login, Register, Profile, CreateCourse }
