4fde73a055d8b2e8273c70754e7eb4a3330cd4fb
Jasticar <jasmin.yap06@gmail.com>
2022-10-24 03:56:12 +0800

    feat: created course creation page and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Dashboard, Error, ForgetResetPwd, Login, Register, Profile } from './pages/index'
+import { Dashboard, Error, ForgetResetPwd, Login, Register, Profile, CreateCourse } from './pages/index'
 import { Footer, Navbar } from './components/index'
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/createcourse" element={<CreateCourse />} />
             </Routes>
             <Footer />
         </BrowserRouter>
