c03436eead3c7ec059e0de02b54be88b6dac2180
Jasticar <jasmin.yap06@gmail.com>
2022-10-24 14:51:57 +0800

    feat: finished form for basic course info

--- client/src/pages/CreateCourse.jsx
+++ client/src/pages/CreateCourse.jsx
@@ -1,30 +1,25 @@
-import { React } from 'react'
-import { useForm } from 'react-hook-form'
+import { React, useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated'
 import { languageOptions } from '../utils/Constants'
 
 const CreateCourse = () => {
+    const animatedComponents = makeAnimated()
+
     const {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm()
+        control,
+    } = useForm({
+        mode: 'onBlur',
+    })
 
-    const topicOptions = null // get list from get all topics API
+    const topicOptions = languageOptions // get list from get all topics API
 
     const onSubmit = (data) => {
         console.log(data)
-    }
-
-    const getLanguageList = () => {
-        return languageOptions.map((language) => {
-            return <option value={language.value}>{language.label}</option>
-        })
-    }
-
-    const getTopicsList = () => {
-        return topicOptions.map((topic) => {
-            return <option value={topic.value}>{topic.label}</option> // to be changed
-        })
     }
 
     return (
@@ -32,12 +27,12 @@ const CreateCourse = () => {
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create a new Course!</h2>
             </div>
-            <div className="flex items-center justify-center">
-                <div className="bg-white mt-10 rounded-lg">
-                    <div className="flex auto-rows-auto auto-cols-auto grid-flow-row p-4">
-                        <form onSubmit={handleSubmit(onSubmit)} className="-wfull max-w-lg">
-                            <div className="flex flex-wrap mb-6">
-                                <div className="w-full p-3 mb-6 md:mb-0">
+            <div className="flex bg-white mt-10 rounded-lg w-full justify-center">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex p-4 w-3/4">
+                    <div className="flex flex-col w-full">=
+                        <div className="flex flex-row w-full">
+                            <div className="flex flex-col w-1/2">
+                                <div className="p-3">
                                     <label
                                         className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                         for="courseName"
@@ -56,27 +51,7 @@ const CreateCourse = () => {
                                         <span className="text-sm text-red-500">{errors.courseName.message}</span>
                                     ) : null}
                                 </div>
-                                <div className="w-full p-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        for="language"
-                                    >
-                                        Language
-                                    </label>
-                                    <select
-                                        className="w-full border border-slate-300 rounded-md p-2"
-                                        id="language"
-                                        {...register('language', {
-                                            required: 'Please select the language the course is taught in',
-                                        })}
-                                    >
-                                        {getLanguageList()}
-                                    </select>
-                                    {errors.language ? (
-                                        <span className="text-sm text-red-500">{errors.language.message}</span>
-                                    ) : null}
-                                </div>
-                                <div className="w-full p-3 mb-6 md:mb-0">
+                                <div className="p-3">
                                     <label
                                         className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                         for="courseDuration"
@@ -95,7 +70,39 @@ const CreateCourse = () => {
                                         <span className="text-sm text-red-500">{errors.courseDuration.message}</span>
                                     ) : null}
                                 </div>
-                                <div className="w-full p-3 mb-6 md:mb-0">
+                            </div>
+                            <div className="flex flex-col w-1/2">
+                                <div className="p-3">
+                                    <label
+                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                        for="language"
+                                    >
+                                        Language
+                                    </label>
+                                    <Controller
+                                        name="language"
+                                        control={control}
+                                        rules={{
+                                            required: 'Please select the language the course is taught in',
+                                        }}
+                                        render={({ field }) => {
+                                            return (
+                                                <Select
+                                                    {...field}
+                                                    isClearable={true}
+                                                    options={languageOptions}
+                                                    isSearchable={true}
+                                                    placeholder="Select a language"
+                                                    className="w-full"
+                                                />
+                                            )
+                                        }}
+                                    />
+                                    {errors.language ? (
+                                        <span className="text-sm text-red-500">{errors.language.message}</span>
+                                    ) : null}
+                                </div>
+                                <div className="p-3">
                                     <label
                                         className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                         for="coursePrice"
@@ -114,28 +121,77 @@ const CreateCourse = () => {
                                         <span className="text-sm text-red-500">{errors.coursePrice.message}</span>
                                     ) : null}
                                 </div>
-                                <div className="w-full p-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        for="topics"
-                                    >
-                                        Relevant Topics
-                                    </label>
-                                    <select
-                                        className="w-full border border-slate-300 rounded-md p-2"
-                                        id="topics"
-                                        multiple
-                                        {...register('topics', {
-                                            required: 'Please select at least one relevant topic',
-                                        })}
-                                    >
-                                        {getLanguageList()}
-                                    </select>
-                                    {errors.topics ? (
-                                        <span className="text-sm text-red-500">{errors.topics.message}</span>
-                                    ) : null}
-                                </div>
                             </div>
+                        </div>
+                        {/* topics row */}
+                        <div className="flex flex-row">
+                            <div className="p-3 w-full">
+                                <label
+                                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                    for="topics"
+                                >
+                                    Relevant Topics
+                                </label>
+                                <Controller
+                                    name="topics"
+                                    control={control}
+                                    rules={{
+                                        required: 'Please select up to 5 topics relevant to this course',
+                                        validate: (value) => 
+                                            value.length <= 5 || 'Please only select 5 relevant topics',
+                                    }}
+                                    render={({ field }) => {
+                                        return (
+                                            <Select
+                                                {...field}
+                                                isClearable={true}
+                                                isMulti
+                                                options={topicOptions}
+                                                isSearchable={true}
+                                                closeMenuOnSelect={false}
+                                                components={animatedComponents}
+                                                placeholder="Select a topic"
+                                                className="w-full"
+                                            />
+                                        )
+                                    }}
+                                />
+                                {errors.topics ? (
+                                    <span className="text-sm text-red-500">{errors.topics.message}</span>
+                                ) : null}
+                            </div>
+                        </div>
+                        {/* description row */}
+                        <div className="flex flex-row">
+                            <div className="p-3 w-full">
+                                <label
+                                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                    for="courseDesc"
+                                >
+                                    Description
+                                </label>
+                                <textarea
+                                    className="w-full border border-slate-300 rounded-md p-2"
+                                    id="courseDesc"
+                                    {...register('courseDesc', {
+                                        required: 'Please enter a short description of the course',
+                                    })}
+                                />
+                                {errors.courseDesc ? (
+                                    <span className="text-sm text-red-500">{errors.courseDesc.message}</span>
+                                ) : null}
+                            </div>
+                        </div>
+                        {/* course materials section */}
+                        <div className="flex flex-row">
+                            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                                <h3 className="mt-6 text-center text-xl font-extrabold text-gray-900">
+                                    Upload Course Material
+                                </h3>
+                            </div>
+                        </div>
+                        {/* button row */}
+                        <div className="flex flex-row">
                             <div className="flex items-center justify-between">
                                 <button
                                     className="shadow focus:shadow-outline focus:outline-none bg-navbarfooter font-bold py-2 px-4 rounded"
@@ -144,9 +200,9 @@ const CreateCourse = () => {
                                     Submit
                                 </button>
                             </div>
-                        </form>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     )
