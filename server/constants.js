30efa4b2d02721174eb3c2fb8916bd16900191a9
Rui-SK <66718507+Rui-SK@users.noreply.github.com>
2022-10-29 03:42:57 +0800

    feat: changed email_template_deactivate message

--- server/constants.js
+++ server/constants.js
@@ -12,4 +12,6 @@ export const email_template = (token) =>
 `http://localhost:5000/api/users/resetPassword/${token}\n\n` +
 'If you did not request this, please ignore this email and your password will remain unchanged.\n'
 
-export const email_template_deactivate = 'This account has been successfully deactivated as of ' + new Date()
+export const email_template_deactivate = 'This account has been successfully deactivated as of ' + new Date() + "\n\n" +
+'If you wish to reactivate your account, please contact our support at testingpurposes706@gmail.com within 30 days \n\n' +
+'After 30 days your account will no longer be able to be reactivated'
