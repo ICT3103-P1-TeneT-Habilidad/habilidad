f71d532c39517236526c8a3b8cf4f0cdf87e84f6
Z-Jian <2002689@sit.singaporetech.edu.sg>
2022-10-29 08:57:40 +0800

    feat: added ip request dependency and ip into log message

--- server/utils/logMessage.js
+++ server/utils/logMessage.js
@@ -1,8 +1,9 @@
+import requestIp from 'request-ip'
+
 export class LogMessage {
     constructor(statusCode, req) {
         const timestamp = req.headers['x-timestamp']
-        this.msg = `${timestamp},${statusCode},${req.method}${req.baseUrl}`
-        // this.msg = `${req.headers.x - timestamp},${statusCode},${req.ip},${req.method}/${req.baseUrl}`
+        this.msg = `${timestamp},${statusCode},${requestIp.getClientIp(req)},${req.method}${req.baseUrl}`
         this.msgSize = Buffer.byteLength(this.msg, "utf-8")
     }
 
