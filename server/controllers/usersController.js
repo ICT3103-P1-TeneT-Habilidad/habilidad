919e7ac07e327e0cc6ec21cd39d4cb8a21477a16
Z-Jian <2002689@sit.singaporetech.edu.sg>
2022-11-01 14:01:11 +0800

    feat: added hash id

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,7 +1,7 @@
 import crypto from 'crypto'
 // import services
 import { addRefreshTokenToWhitelist } from '../services/refreshTokens.js'
-import { findUserbyUserId, findUserByEmail, deActivateUser } from '../services/user.js'
+import { findUserbyUserId, findUserByEmail, deActivateUser, findUserByUsername, storeNewUser } from '../services/user.js'
 import { findEmailToken, replaceEmailToken, saveEmailToken } from '../services/emailToken.js'
 // import constants
 import { email_template, email_template_deactivate } from '../constants.js'
--- server/utils/hash.js
+++ server/utils/hash.js
@@ -1,5 +1,32 @@
 import crypto from 'crypto'
+import { Response } from '../responses/response.js'
+
+// hash constant
+const idSecrets = {
+    course: process.env.COURSE_ID_SECRET,
+    topic: process.env.TOPIC_ID_SECRET
+}
 
 export const hashToken = (token) => {
     return crypto.createHash('sha512').update(token).digest('hex')
+}
+
+export const hashId = (id, type) => {
+    const secret = idSecrets[type]
+
+    if (!secret) throw new Response('Internal Server Error', 'res_internalServer')
+
+    return crypto.createHmac('sha512', secret)
+        .update(id)
+        .digest('hex')
+}
+
+export const decipherId = (id, type) => {
+    const secret = idSecrets[type]
+
+    if (!secret) throw new Response('Internal Server Error', 'res_internalServer')
+
+    return crypto.createHmac('sha512', secret)
+        .update(id)
+        .digest('hex')
 }
