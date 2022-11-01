6b8a8f6eb64c256905408807735335008197288c
xcxpcxkes <77536236+xcxpcxkes@users.noreply.github.com>
2022-11-01 14:36:30 +0800

    feat: add authentication to transaction routes

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { isAuthenticate } from '../controllers/authController.js'
 import { purchaseOneCourse } from '../controllers/transactionController.js'
+import { isRoleStudent } from '../middleware/checkRole.js'
 
 const router = express.Router()
 
 // Get transaction by Students
-router.route('/transactionsByStudent').get(purchaseOneCourse)
+router.route('/transactionsByStudent').get(isAuthenticate, isRoleStudent, purchaseOneCourse)
 
 export default router
