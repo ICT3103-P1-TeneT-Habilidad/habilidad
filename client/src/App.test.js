57955783b22c8983140f847051efd87661ca52d1
Jasticar <jasmin.yap06@gmail.com>
2022-11-01 10:28:46 +0800

    test: testing jenkins build error fix

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,11 +7,11 @@
 //   expect(linkElement).toBeInTheDocument();
 // });
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+})
