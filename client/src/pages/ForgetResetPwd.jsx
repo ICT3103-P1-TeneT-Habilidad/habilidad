e347719d6307029143240ed2bbd2d64eda8d99f4
xcxpcxkes <77536236+xcxpcxkes@users.noreply.github.com>
2022-11-01 21:49:19 +0800

    style: removed username from resetpwd

--- client/src/pages/ForgetResetPwd.jsx
+++ client/src/pages/ForgetResetPwd.jsx
@@ -13,31 +13,15 @@ export default function ForgetResetPwd() {
     return (
         <div className="min-h-screen bg-background flex flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Forget or Reset Password</h2>
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 mb-4">Forget Password</h2>
+                <span className="text-justify text-gray-600 text-md">
+                    You will receive an email password reset link if you have an account registered with Habilidad
+                </span>
             </div>
 
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
                     <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-                        <div>
-                            <label htmlFor="username" className="block text-sm font-medium text-gray-700">
-                                Username
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                    id="username"
-                                    name="username"
-                                    type="text"
-                                    placeholder="Enter Username"
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                    {...register('username', { required: 'Please enter your username' })}
-                                />
-                                {errors.username ? (
-                                    <span className="text-sm text-red-500">{errors.username.message}</span>
-                                ) : null}
-                            </div>
-                        </div>
-
                         <div>
                             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                                 Email
