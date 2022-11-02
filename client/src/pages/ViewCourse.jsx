092875967ed16b795c8d75cc7af40aa3a3e913fc
Jasticar <jasmin.yap06@gmail.com>
2022-11-02 21:43:59 +0800

    feat: completed instructor view course

--- client/src/pages/ViewCourse.jsx
+++ client/src/pages/ViewCourse.jsx
@@ -10,11 +10,21 @@ const ViewCourse = () => {
         description: 'test_course_1_des',
         language: 'ENGLISH',
         courseMaterial: [
-            { title: 'first', url: 'https://media.giphy.com/media/XGxlscqR85DNeRRIII/giphy.gif', order: 1 },
+            { title: 'Basics of React', url: 'https://www.youtube.com/embed/LRVVvmYg9Ls', order: 2 },
             {
-                title: 'second',
-                url: 'https://giphy.com/gifs/kill-me-now-IIozzNpFTvIGc',
-                order: 2,
+                title: 'Death 1 to React',
+                url: 'https://www.youtube.com/embed/HE74FKFZrtI',
+                order: 1,
+            },
+            {
+                title: 'Death 2 to React',
+                url: 'https://res.cloudinary.com/drznyznmo/video/upload/v1667394035/video_2022-11-02_20-58-06_ae87rs.mp4',
+                order: 4,
+            },
+            {
+                title: 'Death 3 to React',
+                url: 'https://www.youtube.com/embed/sSsMsSrXWl4',
+                order: 3,
             },
         ],
         topicCourse: [
@@ -30,8 +40,26 @@ const ViewCourse = () => {
         ],
     }
 
-    const sortCourseMaterials = () => {
-        
+    const sortCourseMaterials = (materials) => {
+        const sortedMaterials = []
+        console.log(materials)
+        materials.forEach((element) => {
+            if (sortedMaterials.length === 0) {
+                sortedMaterials.push(element)
+            } else {
+                for (var i = 0; i < sortedMaterials.length; i++) {
+                    if (element.order < sortedMaterials[i].order) {
+                        sortedMaterials.splice(i, 0, element)
+                        break
+                    }
+                }
+                if (!sortedMaterials.includes(element)) {
+                    sortedMaterials.push(element)
+                }
+            }
+        })
+        console.log(sortedMaterials)
+        return sortedMaterials
     }
 
     return (
@@ -39,7 +67,7 @@ const ViewCourse = () => {
             <div className="bg-white rounded-lg px-6 py-10 mx-auto">
                 <div className="lg:-mx-6 mb-5 lg:flex lg:items-center">
                     <img
-                        className="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96"
+                        className="object-scale-down w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96"
                         src={courseData.imageUrl}
                         alt=""
                     />
@@ -56,22 +84,35 @@ const ViewCourse = () => {
                             <p className="text-sm text-grey-500 tracking-wide">
                                 Related Topics: &ensp;
                                 {courseData.topicCourse.map((topicData) => (
-                                    <a>{topicData.topic.topicName} &ensp;</a>
+                                    <p>{topicData.topic.topicName} &ensp;</p>
                                 ))}
                             </p>
                         </div>
                     </div>
                 </div>
-
                 <hr />
                 <div className="lg:-mx-6 lg:flex lg:items-center">
-                    <div className="mt-6 lg:w-full lg:mt-0 lg:mx-6">
-                        <p className="mt-3 text-xl text-gray-800 md:text-xl">Course Materials</p>
-                        <div>
-                            {
-                                sortCourseMaterials(courseData.courseMaterial)
-
-                            }
+                    <div className="m-6 lg:w-full lg:mt-0 lg:mx-6">
+                        <p className="mt-3 p-4 text-2xl text-gray-800 md:text-2xl">Course Materials</p>
+                        <div class="flex flex-wrap">
+                            {sortCourseMaterials(courseData.courseMaterial).map((material) => (
+                                <div class="lg:w-1/3 sm:w-1/2 p-4">
+                                    <h2 class="tracking-wide font-medium text-grey-500 mb-1">
+                                        Lesson {material.order}: {material.title}
+                                    </h2>
+                                    <div class="flex">
+                                        <iframe
+                                            width="560"
+                                            height="315"
+                                            src={material.url}
+                                            title={material.title}
+                                            frameborder="0"
+                                            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                            allowfullscreen="true"
+                                        ></iframe>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
