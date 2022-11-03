e6eb05c44c5f5f19f19b17e1b9ff04181a52e3f8
xcxpcxkes <77536236+xcxpcxkes@users.noreply.github.com>
2022-11-03 17:52:14 +0800

    feat: grouped the data by alphabets

--- client/src/pages/AllTopics.jsx
+++ client/src/pages/AllTopics.jsx
@@ -21,17 +21,29 @@ const AllTopics = () => {
         console.log(newTopics)
     }
 
+    let groupedData = topics.reduce((a, b) => {
+        let group = b.topicName[0]
+
+        if(!a[group]) a[group] = {group, data: [b]}
+        else a[group].data.push(b)
+        return a
+    }, {})
+
+    let result = Object.values(groupedData)
+
+    console.log(result)
+
     return (
         topics && (
             <div className="min-h-screen bg-background">
                 <div className="px-4 py-4 mx-24 bg-background space-y-2 mr-24">
                     {sortTopicsByName(topics)}
-                    {topics.map((topic) => (
-                        <div key={topic.topicName} className="tw-flex tw-flex-col tw-p-2">
+                    {result.map((one) => (
+                        <div key={one.group} className="tw-flex tw-flex-col tw-p-2">
                             <div className="tw-border tw-border-divider tw-bg-divider tw-text-grey-base tw-p-4 tw-w-24 tw-h-12 tw-text-center tw-font-medium">
-                                {topic.topicName}
+                                {one.group}
                             </div>
-                            <TopicCard data={topics} />
+                            <TopicCard data={one.data} />
                         </div>
                     ))}
                 </div>
