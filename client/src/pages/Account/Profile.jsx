6da27de00f9a49022596edebf33308640716898e
xcxpcxkes <77536236+xcxpcxkes@users.noreply.github.com>
2022-11-04 20:05:21 +0800

    feat: consistent display of user details

--- client/src/pages/Account/Profile.jsx
+++ client/src/pages/Account/Profile.jsx
@@ -3,8 +3,7 @@ import { useForm } from 'react-hook-form'
 import { useAppContext } from '../../context/appContext'
 
 const Profile = () => {
-
-    const {getUserDetails, user_details} = useAppContext()
+    const { getUserDetails, user_details } = useAppContext()
 
     const [isDisabled, setIsDisabled] = useState(true)
     const [hasChanged, setHasChanged] = useState(false)
@@ -17,17 +16,20 @@ const Profile = () => {
         reset,
     } = useForm({
         defaultValues: {
-            role: user_details?.role,
             email: user_details?.email,
             phoneNumber: user_details?.phoneNumber,
             name: user_details?.name,
-            username: user_details?.username
-        }
+            password: '12345678',
+        },
     })
 
     useEffect(() => {
         getUserDetails()
     }, [])
+
+    useEffect(() => {
+        reset(user_details)
+    }, [user_details])
 
     const password = useRef({})
     password.current = watch('password', '')
@@ -101,20 +103,53 @@ const Profile = () => {
 
     const ShowPasswordBlock = () => {
         return (
-            <div className="w-full p-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="re_password">
-                    Re-enter Password
-                </label>
-                <input
-                    className="w-full border border-slate-300 rounded-md p-2"
-                    id="re_password"
-                    type="password"
-                    {...register('re_password', {
-                        validate: (value) => value === password.current || 'The passwords do not match',
-                    })}
-                />
-                {errors.re_password ? <span className="text-sm text-red-500">{errors.re_password.message}</span> : null}
-            </div>
+            <>
+                <div className="w-full p-3 mb-6 md:mb-0">
+                    <label
+                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="password"
+                    >
+                        Password
+                    </label>
+                    <input
+                        className="w-full border border-slate-300 rounded-md p-2"
+                        id="password"
+                        name="password"
+                        type="password"
+                        {...register('password', {
+                            onChange: (e) => {
+                                if (e.target.value === '') {
+                                    setHasChanged(false)
+                                } else {
+                                    setHasChanged(true)
+                                }
+                            },
+                            validate: (value) =>
+                                value.length >= 8 || value.length === 0 || 'Password should have minimum 8 characters',
+                        })}
+                    />
+                    {errors.password ? <span className="text-sm text-red-500">{errors.password.message}</span> : null}
+                </div>
+                <div className="w-full p-3 mb-6 md:mb-0">
+                    <label
+                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="re_password"
+                    >
+                        Re-enter Password
+                    </label>
+                    <input
+                        className="w-full border border-slate-300 rounded-md p-2"
+                        id="re_password"
+                        type="password"
+                        {...register('re_password', {
+                            validate: (value) => value === password.current || 'The passwords do not match',
+                        })}
+                    />
+                    {errors.re_password ? (
+                        <span className="text-sm text-red-500">{errors.re_password.message}</span>
+                    ) : null}
+                </div>
+            </>
         )
     }
 
@@ -122,103 +157,82 @@ const Profile = () => {
         // main div for background
         <div className="min-h-screen bg-background">
             <div className="flex items-center justify-center pt-10 flex-col">
-                <h1 className="text-gray-800 font-semibold text-xl mt-5">NAME</h1>
-                <h1 className="text-gray-500 text-sm">ROLE</h1>
+                <h1 className="text-gray-800 font-semibold text-xl mt-5">{user_details.username}</h1>
+                <h1 className="text-gray-500 text-sm">{user_details.role}</h1>
             </div>
             <div className="flex items-center justify-center">
                 <div className="bg-white w-1/3 mt-10 rounded-lg">
                     {isDisabled ? <ShowEditButton /> : null}
                     <div className="flex auto-rows-auto auto-cols-auto grid-flow-row p-4">
-                        <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-lg">
-                            <div className="flex flex-wrap -mx-3 mb-6">
-                                <div className="w-full md:w-1/2 p-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        htmlFor="name"
-                                    >
-                                        Name
-                                    </label>
-                                    <input
-                                        disabled={isDisabled}
-                                        className="w-full border border-slate-300 rounded-md p-2"
-                                        id="name"
-                                        type="text"
-                                        {...register('name', { required: 'Please enter your name' })}
-                                    />
-                                    {errors.name ? (
-                                        <span className="text-sm text-red-500">{errors.name.message}</span>
-                                    ) : null}
+                        {user_details && (
+                            <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-lg">
+                                <div className="flex flex-wrap -mx-3 mb-6">
+                                    <div className="w-full md:w-1/2 p-3 mb-6 md:mb-0">
+                                        <label
+                                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                            htmlFor="name"
+                                        >
+                                            Name
+                                        </label>
+                                        <input
+                                            disabled={isDisabled}
+                                            className="w-full border border-slate-300 rounded-md p-2"
+                                            id="name"
+                                            type="text"
+                                            {...register('name', { required: 'Please enter your name' })}
+                                        />
+                                        {errors.name ? (
+                                            <span className="text-sm text-red-500">{errors.name.message}</span>
+                                        ) : null}
+                                    </div>
+                                    <div className="w-full md:w-1/2 p-3 mb-6 md:mb-0">
+                                        <label
+                                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                            htmlFor="phoneNumber"
+                                        >
+                                            Phone Number
+                                        </label>
+                                        <input
+                                            disabled={isDisabled}
+                                            className="w-full border border-slate-300 rounded-md p-2"
+                                            id="phoneNumber"
+                                            type="tel"
+                                            {...register('phoneNumber', { required: 'Please enter your phone number' })}
+                                        />
+                                        {errors.phoneNumber ? (
+                                            <span className="text-sm text-red-500">{errors.phoneNumber.message}</span>
+                                        ) : null}
+                                    </div>
+                                    <div className="w-full p-3 mb-6 md:mb-0">
+                                        <label
+                                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                            htmlFor="email"
+                                        >
+                                            Email Address
+                                        </label>
+                                        <input
+                                            disabled={isDisabled}
+                                            className="w-full border border-slate-300 rounded-md p-2"
+                                            id="email"
+                                            type="email"
+                                            {...register('email', { required: 'Please enter your email address' })}
+                                        />
+                                        {errors.email ? (
+                                            <span className="text-sm text-red-500">{errors.email.message}</span>
+                                        ) : null}
+                                    </div>
+                                    {isDisabled ? null : <ShowPasswordBlock />}
                                 </div>
-                                <div className="w-full md:w-1/2 p-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        htmlFor="phoneNumber"
-                                    >
-                                        Phone Number
-                                    </label>
-                                    <input
-                                        disabled={isDisabled}
-                                        className="w-full border border-slate-300 rounded-md p-2"
-                                        id="phoneNumber"
-                                        type="tel"
-                                        {...register('phoneNumber', { required: 'Please enter your phone number' })}
-                                    />
-                                    {errors.phoneNumber ? (
-                                        <span className="text-sm text-red-500">{errors.phoneNumber.message}</span>
-                                    ) : null}
+                                {isDisabled ? null : <ShowProfileButtons />}
+                            </form>
+                        )}
+                        {!user_details && (
+                            <div>
+                                <div className="text-center p-3">
+                                    <span className="spinner-border spinner-border-lg align-center"></span>
                                 </div>
-                                <div className="w-full p-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        htmlFor="email"
-                                    >
-                                        Email Address
-                                    </label>
-                                    <input
-                                        disabled={isDisabled}
-                                        className="w-full border border-slate-300 rounded-md p-2"
-                                        id="email"
-                                        type="email"
-                                        {...register('email', { required: 'Please enter your email address' })}
-                                    />
-                                    {errors.email ? (
-                                        <span className="text-sm text-red-500">{errors.email.message}</span>
-                                    ) : null}
-                                </div>
-                                <div className="w-full p-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        htmlFor="password"
-                                    >
-                                        Password
-                                    </label>
-                                    <input
-                                        disabled={isDisabled}
-                                        className="w-full border border-slate-300 rounded-md p-2"
-                                        id="password"
-                                        type="password"
-                                        {...register('password', {
-                                            onChange: (e) => {
-                                                if (e.target.value === '') {
-                                                    setHasChanged(false)
-                                                } else {
-                                                    setHasChanged(true)
-                                                }
-                                            },
-                                            validate: (value) =>
-                                                value.length >= 8 ||
-                                                value.length === 0 ||
-                                                'Password should have minimum 8 characters',
-                                        })}
-                                    />
-                                    {errors.password ? (
-                                        <span className="text-sm text-red-500">{errors.password.message}</span>
-                                    ) : null}
-                                </div>
-                                {hasChanged ? <ShowPasswordBlock /> : null}
                             </div>
-                            {isDisabled ? null : <ShowProfileButtons />}
-                        </form>
+                        )}
                     </div>
                 </div>
             </div>
