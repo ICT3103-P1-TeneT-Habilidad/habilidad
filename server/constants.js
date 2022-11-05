01150c5763436d40a93cb636ae25779b6e3ad4c2
Jasticar <jasmin.yap06@gmail.com>
2022-11-06 01:08:05 +0800

    feat: updated constants

--- server/constants.js
+++ server/constants.js
@@ -4,12 +4,12 @@ dotenv.config({ path: './.env' })
 
 export const prod_url = 'https://habilidad.tk'
 
-export const dev_url = 'http://localhost:5000'
+export const dev_url = 'http://172.16.1.3:5000'
 
 export const email_template = (token) =>
     'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
     'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n' +
-    `http://localhost:5000/api/users/resetPassword/${token}\n\n` +
+    `http://172.16.1.3:5000/api/users/resetPassword/${token}\n\n` +
     'If you did not request this, please ignore this email and your password will remain unchanged.\n'
 
 export const email_template_deactivate = 'This account has been successfully deactivated as of ' + new Date() + "\n\n" +
