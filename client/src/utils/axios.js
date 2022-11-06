e55f184acc98a0665f50944d58609dfeb45a232d
Jasticar <jasmin.yap06@gmail.com>
2022-11-06 16:05:11 +0800

    feat: cors error

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,7 +4,8 @@ const instance = axios.create({
     baseURL: 'http://172.16.1.3:5000',
     // baseURL: 'http://localhost:5000',
     headers: {
-        "Access-Control-Allow-Origin": '*'
+        "Access-Control-Allow-Origin": '*',
+        mode: 'cors',
     }
 });
 
