c663e2501818cce9da8448ec8cd2cc4619e13ec8
Jasticar <jasmin.yap06@gmail.com>
2022-11-06 11:03:23 +0800

    test: testing usser-defined network

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://172.16.1.3:5000',
+    baseURL: 'http://habilidad_server:5000',
+    // baseURL: 'http://localhost:5000',
     headers: {
         "Access-Control-Allow-Origin": '*'
     }
