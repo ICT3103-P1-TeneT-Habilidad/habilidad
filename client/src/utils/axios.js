24785fa9a1b057b222e462bc6ebb9a0af62c281b
Jasticar <jasmin.yap06@gmail.com>
2022-11-06 20:43:12 +0800

    feat: deployment

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'habilidad_server:5000',
+    baseURL: 'https://api.habilidad.tk',
     // baseURL: 'http://localhost:5000',
     headers: {
         'Access-Control-Allow-Origin': '*',
