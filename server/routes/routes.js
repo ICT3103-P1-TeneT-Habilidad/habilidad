f7da210c30e4c33550d0467e995228bd8fec877e
Jasticar <jasmin.yap06@gmail.com>
2022-11-07 01:49:14 +0800

    fix: increased rate limiter

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,7 +9,7 @@ const router = express.Router()
 
 const apiLimiter = rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10, // limit each IP to 5 requests per 'window'
+    max: 100, // limit each IP to 5 requests per 'window'
     message: 'Too many requests from this IP, please try again after 15 minutes',
 })
 
